e59853be78702ef9ea1bbbf96444ada256a68aff
Ammar <ammarferchichi7@gmail.com>
2022-06-17 15:01:23 +0100

    render data from db

--- client/src/App.js
+++ client/src/App.js
@@ -1,66 +1,13 @@
-import { useState } from "react";
 import "./App.css";
-import Axios from "axios";
+import EmployeeList from "./components/EmployeeList";
+import Form from "./components/Form";
 
 function App() {
-  const [name, setName] = useState("");
-  const [age, setAge] = useState(0);
-  const [country, setCountry] = useState("");
-  const [position, setPosition] = useState("");
-  const [wage, setWage] = useState(0);
-
-  const addEmployee = () => {
-    Axios.post("http://localhost:3001/employees/", {
-      name: name,
-      age: age,
-      country: country,
-      position: position,
-      wage: wage,
-    }).then(console.log("success"));
-  };
 
   return (
     <div className="App">
-      <div className="information">
-        <label>Name : </label>
-        <input
-          type="text"
-          onChange={(event) => {
-            setName(event.target.value);
-          }}
-        />
-        <label>Age : </label>
-        <input
-          type="number"
-          onChange={(event) => {
-            setAge(event.target.value);
-          }}
-        />
-        <label>Country : </label>
-        <input
-          type="text"
-          onChange={(event) => {
-            setCountry(event.target.value);
-          }}
-        />
-        <label>Position : </label>
-        <input
-          type="text"
-          onChange={(event) => {
-            setPosition(event.target.value);
-          }}
-        />
-        <label>Wage(year) : </label>
-        <input
-          type="number"
-          onChange={(event) => {
-            setWage(event.target.value);
-          }}
-        />
-        <button className="form-button" type="submit" onClick={addEmployee}>
-          Add Employee
-        </button>
-      </div>
+     <Form/>
+     <EmployeeList/>
     </div>
   );
 }
---
+++ client/src/components/EmployeeList.js
@@ -0,0 +1,45 @@
+import Axios from "axios";
+import "./Form.css";
+import React, { useState, useEffect } from "react";
+import "./EmployeeList.css";
+
+const EmployeeList = () => {
+  const [employees, setEmployees] = useState();
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/employees/").then((res) => {
+      setEmployees(res.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <table className="zui-table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Country</th>
+            <th>Position</th>
+            <th>Wage</th>
+          </tr>
+        </thead>
+        {(employees || []).map((val, key) => {
+          return (
+            <tbody>
+              <tr>
+                <td>{val.name}</td>
+                <td>{val.age}</td>
+                <td>{val.country}</td>
+                <td>{val.position}</td>
+                <td>{val.wage}</td>
+              </tr>
+            </tbody>
+          );
+        })}
+      </table>
+    </div>
+  );
+};
+
+export default EmployeeList;
